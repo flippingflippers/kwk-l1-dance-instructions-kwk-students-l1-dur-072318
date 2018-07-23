0ef69ed16aeaffc95df7e61ca8c0438696d82c86
flippingflippers <citrussunshine2004@gmail.com>
2018-07-23 20:03:09 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,4 +1,8 @@
 
 # Code your methods below
-def lower body
-  plant legs far apart, bend knees slightly
+def footwork
+  puts "plant legs far apart, bend knees slightly and keep posture loose"
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+end
