3dff92218f336091aa9559e48693e2b2def5d3bd
flippingflippers <citrussunshine2004@gmail.com>
2018-07-23 20:28:23 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,33 +1,34 @@
 
 # Code your methods below
 def footwork
-  puts "lift right foot"
-  puts "return right foot to the ground"
-  puts "finishing with a small skip-step backward"
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
 end
 
 def skipstep
-  puts "lower left foot to the ground"
-  puts "bounce left foot back up slighlty, kicking it a few inches back"
-  puts "left,right, left and left"
-  puts "lower right foot to the ground"
-  puts "bounce right foot back up slightly, kicking it a few inches back"
-  puts "right, left, right and right"
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slighlty, kicking it a few inches back"
+  puts "Left,right, left and left"
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
+  puts "Right, left, right and right"
 end
 
 def bob_the_reins
-  puts "holding your arms out in front of you, straight and at chest level"
-  puts "cross your right wrist over your left and hold them together"
-  puts "lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement os repeated eight times"
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement os repeated eight times"
 end
 
  def lasso
-   puts "start arms movement by holding left arm so the nack of your wrist is near your chin, pointing your left elbow straight left"
-   puts "lift right arm up and point right elbow diagonally to the right"
-   puts "raise right forearm so that it points straight up, and whip ot on small circles to the beat of the song, as if you were a cowboy holding a rope lasso. this movement is repeated eight times as well"
+   puts "Start arms movement by holding left arm so the nack of your wrist is near your chin, pointing your left elbow straight left"
+   puts "Lift right arm up and point right elbow diagonally to the right"
+   puts "Raise right forearm so that it points straight up, and whip ot on small circles to the beat of the song, as if you were a cowboy holding a rope lasso. this movement is repeated eight times as well"
  end
+ 
  def starting_stance
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Plant their legs far apart, bend knees slightly and keep posture loose"
  end
  starting_stance
  footwork
