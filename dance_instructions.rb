8571c65aabd59af59c4d10342044b13b6ff7362c
flippingflippers <citrussunshine2004@gmail.com>
2018-07-23 20:15:15 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,3 +6,25 @@ def footwork
   puts "return right foot to the ground"
   puts "finishing with a small skip-step backward"
 end
+
+def skip-step
+  puts "lower left foot to the ground"
+  puts "bounce left foot back up slighlty, kicking it a few inches back"
+  puts "left,right, left and left"
+  puts "lower right foot to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts "right, left, right and right"
+end
+
+def bob the reins
+  puts "holding your arms out in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement os repeated eight times"
+end
+
+ def lasso
+   puts "start arms movement by holding left arm so the nack of your wrist is near your chin, pointing your left elbow straight left"
+   puts "lift right arm up and point right elbow diagonally to the right"
+   puts "raise right forearm so that it points straight up, and whip ot on small circles to the beat of the song, as if you were a cowboy holding a rope lasso. this movement is repeated eight times as well"
+ end
+ bob the reins
